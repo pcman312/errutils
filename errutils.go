22de7dd780c32bc960123f8cf75c236f39c9e097
michael.golowka <michael.golowka@logrhythm.com>
2017-06-23 16:03:50 -0600

    Split error and string functions

--- errutils.go
+++ errutils.go
@@ -1,10 +1,15 @@
 package errutils
 
 import (
+	"errors"
 	"strings"
 )
 
-func JoinErrs(errs []error, delim string) string {
+func JoinErrs(delim string, errs... error) error {
+	return errors.New(JoinErrsStr(delim, errs...))
+}
+
+func JoinErrsStr(delim string, errs... error) string {
 	strs := make([]string, len(errs))
 	for i, err := range errs {
 		strs[i] = err.Error()
